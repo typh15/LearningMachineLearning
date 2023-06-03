903a0d2e2ca852b0ea9c2cd8df59896d4026b6c3
Tyler B <typh15@yahoo.com>
2023-06-03 19:17:22 -0400

    Updt

--- MLTesting/first_neural_net.py
+++ MLTesting/first_neural_net.py
@@ -3,17 +3,14 @@ import numpy as np
 from torch import nn
 from torchvision import datasets, transforms
 
-transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Normalize((0.5, ), (0.5, )), ])
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, ), (0.5, )), ])
 
 batch_size = 64
 
-train_set = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/',
-                                  download=True, train=True, transform=transform)
+train_set = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=True, transform=transform)
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
 
-test_set = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/',
-                                 download=True, train=False, transform=transform)
+test_set = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True)
 
 
