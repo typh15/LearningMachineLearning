12455fe6992b52f7a1f03e4a7df2200cab7ccb68
Tyler B <typh15@yahoo.com>
2023-06-04 04:46:40 -0400

    Update first_neural_net.py

--- MLTesting/first_neural_net.py
+++ MLTesting/first_neural_net.py
@@ -4,6 +4,7 @@ from torch import nn, optim
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 
+# This defines a transformation on the grid of training/test images to tensors
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, ), (0.5, )), ])
 
 batch_size = 64
@@ -14,12 +15,14 @@ train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shu
 test_set = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True)
 
+
 class FashionNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.hidden1 = nn.Linear(784, 256)
         self.hidden2 = nn.Linear(256, 128)
-        self.output = nn.Linear(128, 10)
+        self.hidden3 = nn.Linear(128, 64)
+        self.output = nn.Linear(64, 10)
         self.activation = nn.ReLU()
         self.log_softmax = nn.LogSoftmax()
 
@@ -28,32 +31,42 @@ class FashionNetwork(nn.Module):
         x = self.activation(x)
         x = self.hidden2(x)
         x = self.activation(x)
+        x = self.hidden3(x)
+        x = self.activation(x)
         x = self.output(x)
         output = self.log_softmax(x)
         return output
 
 
+#   Instantiating the Network:
 model = FashionNetwork()
 criterion = nn.NLLLoss()
 optimizer = optim.Adam(model.parameters())
 
+# Training the model:
 
-epoch = 10
-#
-# for i in range(epoch):
-#     running_loss = 0
-#     for image, label in train_loader:
-#         optimizer.zero_grad()
-#         image = image.view(image.shape[0], -1)
-#         pred = model(image)
-#         loss = criterion(pred, label)
-#         loss.backward()
-#         optimizer.step()
-#         running_loss += loss.item()
-#     print(f'Training loss: {running_loss / len(train_loader):.4f}')
-#
-tensor_image = train_set.data[0]
-plt.imshow(tensor_image)
+epoch = 6   # How many rounds of training?
+
+for i in range(epoch):
+    running_loss = 0
+    for image, label in train_loader:
+        optimizer.zero_grad()
+        image = image.view(image.shape[0], -1)
+        pred = model(image)
+        loss = criterion(pred, label)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    print(f'Training loss: {running_loss / len(train_loader):.4f}')
+
+
+#  An example of looking at the loaded data:
+#    look at first n-training images...
+def peek(n):
+    for j in range(n):
+        tensor_image = train_set.data[i]
+        plt.imshow(tensor_image)
+        plt.show()
 
 
 
