5edca1da81db63982edeef169a1dcc384fdd089d
linkwin <alanscholar5@gmail.com>
2023-06-04 17:57:35 -0400

    Add more testing results

--- MLTesting/first_neural_net.py
+++ MLTesting/first_neural_net.py
@@ -3,6 +3,7 @@ import numpy as np
 from torch import nn, optim
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
+import sys
 
 # This defines a transformation on the grid of training/test images to tensors
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, ), (0.5, )), ])
@@ -65,11 +66,24 @@ def peek(n):
         plt.show()
 
 
+test_run_count = 20
+fail_count = 0
+pass_count = 0
 # Testing the trained model on the first image-label pair
-test_image = test_set[0][0].view(test_set[0][0].shape[0], -1)   # We need to "unwrap" the image, so it matches the dimension
-prediction = model(test_image)                                  # of the first layer.
-print("Model Prediction: ", prediction)
-print("Actual Label: ", test_set[0][1])
+for i in range(test_run_count):
+    test_image = test_set[i][0].view(test_set[i][0].shape[0], -1)   # We need to "unwrap" the image, so it matches the dimension
+    prediction = model(test_image)                                  # of the first layer.
+    if torch.argmax(prediction).item() == test_set[i][1]:
+        pass_count = pass_count + 1
+    else:
+        fail_count = fail_count + 1
+        print("Failed prediction: ", prediction)
+        print("Expected result: ", test_set[i][1])
+
+print("Tests: ", test_run_count)
+print("Pass: ", pass_count)
+print("Fail: ", fail_count)
+
 
 # What is it trying to classify?
 tensor_image0 = test_set.data[0]
