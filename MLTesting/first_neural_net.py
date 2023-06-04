25cc844d5a715d8ba8ac1c3a7fcc23bd4e9ae450
Tyler B <typh15@yahoo.com>
2023-06-04 14:28:03 -0400

    More Context

--- MLTesting/first_neural_net.py
+++ MLTesting/first_neural_net.py
@@ -19,9 +19,8 @@ test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuff
 class FashionNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.hidden1 = nn.Linear(784, 256)
-        self.hidden2 = nn.Linear(256, 128)
-        self.hidden3 = nn.Linear(128, 64)
+        self.hidden1 = nn.Linear(784, 128)
+        self.hidden2 = nn.Linear(128, 64)
         self.output = nn.Linear(64, 10)
         self.activation = nn.ReLU()
         self.log_softmax = nn.LogSoftmax()
@@ -30,8 +29,6 @@ class FashionNetwork(nn.Module):
         x = self.hidden1(x)
         x = self.activation(x)
         x = self.hidden2(x)
-        x = self.activation(x)
-        x = self.hidden3(x)
         x = self.activation(x)
         x = self.output(x)
         output = self.log_softmax(x)
@@ -45,7 +42,7 @@ optimizer = optim.Adam(model.parameters())
 
 # Training the model:
 
-epoch = 6   # How many rounds of training?
+epoch = 3   # How many rounds of training?
 
 for i in range(epoch):
     running_loss = 0
@@ -60,8 +57,7 @@ for i in range(epoch):
     print(f'Training loss: {running_loss / len(train_loader):.4f}')
 
 
-#  An example of looking at the loaded data:
-#    look at first n-training images...
+#  An example of looking at the first n-training images...
 def peek(n):
     for j in range(n):
         tensor_image = train_set.data[i]
@@ -69,7 +65,16 @@ def peek(n):
         plt.show()
 
 
+# Testing the trained model on the first image-label pair
+test_image = test_set[0][0].view(test_set[0][0].shape[0], -1)   # We need to "unwrap" the image so it matches the dimension
+prediction = model(test_image)                                  # of the first layer.
+print("Model Prediction: ", prediction)
+print("Actual Label: ", test_set[0][1])
 
+# What is it trying to classify?
+tensor_image0 = test_set.data[0]
+plt.imshow(tensor_image)
+plt.show()  # It's a shoe lol
 
 
 
